a59bd561335a9db465b36d4e9cf9dd5f8a04f642
Nitin <ntnr32@gmail.com>
2023-04-05 23:18:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App grid place-content-center">
+      <h1 className='text-xl'>hello world!</h1>
+    </div>
+  )
+}
+
+export default App
